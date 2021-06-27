64045bba60f8a9cc0c8a72c1513192683212a219
Muhammad Zain Ammad <zainammad05@gmail.com>
2021-06-27 13:04:56 +0500

    dummy header and card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import Header from './components/header'
+import Card from './components/card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Card cardTitle = 'Mobiles' img = "https://k.nooncdn.com/cms/pages/20210120/800542152b0966457fa0968e13024ce3/en_cat-module-mobile.png"/>
     </div>
   );
 }
